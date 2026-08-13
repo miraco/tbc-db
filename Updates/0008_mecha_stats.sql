c1f2681559ff6e506f85a73b1a047b67dac3188c
Miraco <daaniel743@gmail.com>
2026-08-14 00:23:19 +0200

    mecha bota

---
+++ Updates/0008_mecha_stats.sql
@@ -0,0 +1,77 @@
+-- ==========================
+-- Mechanar - Normal - Stats Reseach based on TBC Anni PTR
+-- Stats gathered with addon API and MindControle
+-- Also compared with Bestiary values
+-- If NPCs were Imune i used to let them hit me for atleast 500 times to get some good min/max/avg values
+-- All tests were done on a 31342 Armor Tank - 75% for all npc under level 70
+-- ==========================
+
+-- Tempest-Forge Patroller
+-- Damage on 31342 Armor Tank
+-- Level 71
+-- 346-488 avg 420
+
+
+-- Bloodwarder Physician
+-- Level 69 UnitClass  2
+-- MinDmg 1044.61 MaxDmg 1473.01 AP 282 Armor 5235 Speed 2.5
+-- Strength 151 Agi 106 Stam 275 Int 130 SP 120
+-- Bestiary Values
+-- Level DMG
+
+-- Bloodwarder Centurion
+-- Level 70 UnitClass 1
+-- minDMG 1177.07 MaxDmg 1664.37 AP 3404 Armor 6792 Speed 2.5
+-- Strength 162 Agi 127 Stam 307 Int 33 SP 99
+-- Bestiary Values
+-- Level DMG
+
+-- Sunseeker Netherbinder
+-- Level 70 UnitClass 8
+-- MinDmg 916.41 MaxDmg 1329.66 AP 135 Armor 3427 Speed 2.5
+-- Strength 145 Agi 38 Stam 101 Int 327 SP 263
+-- Bestiary Values
+-- Level DMG
+
+-- Arcane Servant
+
+-- Bloodwarder Slayer
+-- Level 70 UnitClass 1
+-- MinDmg 1177.07 MaxDmg 1664.37 AP 304 Armor 6792 Speed 2.5
+-- Strength 162 Agi 127 Stam 307 Int 33 SP 99
+
+-- Sunseeker Engineer
+-- Level 69 unitClass 2
+-- MinDmg 1044.61 MaxDmg 1473.01 AP 282 Armor 5235 Speed 2.5
+-- Strength 151 Agi 106 Stam 275 Int 130 SP 120
+
+-- Mechanar Tinkerer
+-- Level 69 UnitClass 1
+-- MinDmg 362.46 MaxDmg 511.75 AP 298 Armor 6492 Speed 2.8
+-- Strength 159 Agi 122 Stam 301 Int 32 SP 96
+
+
+-- Mechanar Wrecker
+-- Level 70 UnitClass 1
+-- MiNDmg 1458.08 Maxdmg 2061.72 AP 304 Armor 6792 Speed 3.08
+-- Strength 162 Agi 127 Stam 307 Int 33 SP 99
+
+-- Gatewatcher Gyro-Kill
+-- Has a attackspeed buff and armor reduction no dmg test done
+
+-- Tempest-Forge Destroyer
+-- Hitting for 369-518 avg 436 on Tank Druid
+-- Charged Fist for 988 - 2099 189s avg
+
+-- Sunseeker Astromage
+-- Level 69 UnitClass 2
+-- MinDmg 1044.62 MaxDmg 1473.01 AP 282 Armor 5235 Speed 2.5
+-- Strength 151 Agi 106 Stam 275 Int 130 SP 120
+
+-- Sunseeker Engineer
+-- Level 69 UnitClass 2
+-- MinDmg 1044.61 MaxDmg 1473.01 AP 282 Armor 5235 Speed 2.5
+-- Strength 151 Agi 106 Stam 275 Int 130 SP 120
+
+
+
---
+++ Updates/0009_botanica.sql
@@ -0,0 +1,27 @@
+-- ==========================
+-- Botanica - Normal - Stats Reseach based on TBC Anni PTR
+-- Stats gathered with addon API and MindControle
+-- Also compared with Bestiary values
+-- If NPCs were Imune i used to let them hit me for atleast 500 times to get some good min/max/avg values
+-- All tests were done on a 31342 Armor Tank - 75% for all npc under level 70
+-- ==========================
+
+-- Tempest-Forge Peackeeper
+-- Damage on 31342 Armor Tank
+-- Level 70
+-- 
+
+-- Bloodwarder Protector
+-- Level 69 UnitClass 2
+-- MinDmg 1044.61 MaxDmg 1473.01 AP 282 Armor 5235 Speed 2.5
+-- Str 151 Agi 106 Stam 275 Int 130 SP 120
+
+-- Bloodwarder Greenkeeper
+-- Level 70 UnitClass 2
+-- MinDmg 1092.45 MaxDmg 1543.43 AP 286 Armor 5474 Speed 2.5
+-- Str 153 Agi 108 Sta 280 Int 133 SP 122
+
+-- Bloodwarder mender 
+-- Level 69 UnitClass 8
+-- MiNDmg 873.95 MaxDmg 1266.63 AP 133 Armor 3277 Speed 2.5
+-- Str 143 Agi 38 Sta 99 Int 320 SP 257
