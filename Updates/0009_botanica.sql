2920719a64d9081129addcb88d05ab36e29af30d
Miraco <daaniel743@gmail.com>
2026-08-14 22:14:06 +0200

    bota good arka shit

--- Updates/0009_botanica.sql
+++ Updates/0009_botanica.sql
@@ -8,6 +8,7 @@
 
 -- Tempest-Forge Peackeeper
 -- Damage on 31342 Armor Tank
+-- 277-388
 -- Level 70
 -- 
 
@@ -25,3 +26,95 @@
 -- Level 69 UnitClass 8
 -- MiNDmg 873.95 MaxDmg 1266.63 AP 133 Armor 3277 Speed 2.5
 -- Str 143 Agi 38 Sta 99 Int 320 SP 257
+
+-- Bloodfalcon
+-- Level 69
+-- Damage 821-1160 6492 Armor
+
+-- Bloodwarder Falconer
+-- Level 70 UnitClass 1
+-- MinDmg 1177.07 MaxDmg 1664.37 AP 304 Armor 6792 Speed 2.5
+-- Str 162 Agi 127 Stam 307 Int 33 Sp 99
+
+-- Bloodwarder Steward
+-- Level 70 UnitClass 1
+-- MinDmg 1177.07 MaxDmg 1664.37 AP 304 Armor 6792 Speed 2.5
+-- Str 162 Agi 127 Stam 307 Int 33 Sp 99
+
+
+-- Sunseeker Chemist
+-- Level 70 UnitClass 2
+-- MinDmg 1092.45 MaxDmg 1543.43 AP 286 Armor 5474 Speed 2.5
+-- Strength 153 Agi 108 Stam 280 Int 133 Sp 122
+
+
+-- Sunseeker Researcher
+-- Level 70 UnitClass 2
+-- MinDmg 1092.45 MaxDmg 1543.43 AP 286 Armor 5474 Speed 2.5
+-- Strength 153 Agi 108 Stam 280 Int 133 Sp 122
+
+-- Sunseeker Botanist
+-- Level 70 UnitClass 2
+-- MinDmg 1092.45 MaxDmg 1543.43 AP 286 Armor 5474 Speed 2.5
+-- Strength 153 Agi 108 Stam 280 Int 133 Sp 122
+
+
+-- Greater Frayer
+-- U/A
+
+-- Frayer 
+-- U/A
+
+-- Sunseeker Geomancer 
+-- U/A
+
+
+-- Nethervine Inciter
+-- Level 70 UnitClass 1
+-- MinDmg 1177.07 Maxdmg 1664.37 AP 304 Armor 6792 Speed 2.8
+-- Str 162 Agi 127 Stam 307 Int 33 SP 99
+
+
+-- Sunseeker Harvester 
+-- Level 70 UnitClass 2
+-- MinDmg 1092. 45 MaxDmg 1543 AP 286 Armor 5474 Speed 2.5
+-- Strength 153 Agi 108 Stam 280 int 133 SP 122
+
+-- Sunseeker Herbalist
+-- Level 70 UnitClass 1
+-- MinDmg 1177.07 MaxDmg 1664.37 AP 304 Armor 6792 Speed 2.5
+-- Strength 162 Agi 127 Stam 307 Int 307 Int 33 Sp 99
+
+
+-- Sunseeker Channeller
+-- Level 70 unitClass 8
+-- MinDmg 916.41 MaxDmg 1329.66 AP 135 Armor 3427 Speed 2.5
+-- Str 145 Agi 38 Stam 101 Int 327 SP 263
+
+-- Nethervine Reaper
+-- Level 70 UnitClass1
+-- MinDmg 1177.07 MaxDmg 1664.37 AP 304 Armor 6792 Speed 2.8
+-- Strength 162 Agi 127 Stam 307 Int 33 SP 99
+
+-- Nethervine Trickster
+-- Level 70 UnitClass 1
+-- MinDmg 1177.07 MaxDMG 1664.37 AP 304 Armor 6792 Speed 2.8
+-- Str 162 Agi 127 Stam 307 Int 33 SP 99
+
+-- Mutate Fleshlasher
+-- 1514 - 2143 Damage 6125 Armor
+
+-- Sunseeker Gene-Splicer
+-- Level 70 UnitClass 8
+-- MiNDmg 916.41 MaxDmg 1329.66 AP 135 Armor 3427 Speed 2.5
+-- Str 145 Agi 38 Stam 101 Int 327 SP 263
+
+-- Mutate horror
+-- 1177-1665 Damage 6125 Armor 20259 h
+
+-- Mutate Fear-Shrieker
+-- 1177-1665 Damage 6125 Armor
+
+-- Frayer Wildling
+
+-- Bosses
---
+++ Updates/arkatraz_statsxD.sql
@@ -0,0 +1,22 @@
+-- ==========================
+-- Arcatraz - Normal - Stats Reseach based on TBC Anni PTR
+-- Stats gathered with addon API and MindControle
+-- Also compared with Bestiary values
+-- If NPCs were Imune i used to let them hit me for atleast 500 times to get some good min/max/avg values
+-- All tests were done on a 31342 Armor Tank - 75% for all npc under level 70
+-- ==========================
+
+-- Arcatraz Warder
+-- Level 68 UnitClass 2
+-- MinDmg 997.50 MaxDmg 1404.34 AP 276 Armor 4995 Speed 2.5
+-- Str 148 Agi 104 Sta 271 Int 127 SP 118
+
+
+-- Protean horror
+-- Damage on Tank
+
+
+-- Protean Nightmare
+-- 504 - 708
+
+
